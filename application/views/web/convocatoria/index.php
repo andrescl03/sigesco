64ba1262ed144e735f6a32fda67a6048eb191c21
ext.dcarrion <andrescarrion199603@gmail.com>
2024-01-22 23:23:33 -0500

    fix estado postulacion convocatoria

--- application/views/web/convocatoria/index.php
+++ application/views/web/convocatoria/index.php
@@ -7,65 +7,65 @@
         </div>
         <div class="card-body">
             <div class="row">
-            <div class="table-responsive">
-                <table class="table table-hover">
-                    <thead style="background-color:red" class="text-white">
-                        <tr>
-                            <!-- <th class="text-center">#</th>-->
-                            <th class="text-center">NÚMERO</th>
-                            <th class="text-center">TIPO</th>
-                            <th class="text-center">FECHA DE INICIO</th>
-                            <th class="text-center">FECHA DE FIN</th>
-                            <th class="text-center">GRUPOS DE INSCRIPCIÓN</th>
-                            <th class="text-center">ESTADO</th>
-                            <th class="text-center">ACCIONES</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $arrUnicos = [];
-                        $i = 0;
-                        foreach ($datos as $dato) {
-                            if (!in_array($dato['con_id'], $arrUnicos)) {
-                                $arrUnicos[] = $dato['con_id'];
-                                $cadena = $dato['con_id'] . "||0";
-                        ?>
-                                <tr>
-                                    <!--<td class="text-center"><b><?= $i + 1; ?></b></td>-->
-                                    <td class="text-center"><b><?= "CONV-" . sprintf('%04d', $dato['con_numero']) . "-" . $dato['con_anio'] ?></b></td>
-                                    <td class="text-center"><?= $dato['descripcion'] ?></td>
-                                    <td class="text-center"><?= format_date($dato['con_fechainicio'], "d/m/Y") . " " .substr($dato['con_horainicio'], 0, 5) ?> </td>
-                                    <td class="text-center"><?= format_date($dato['con_fechafin'], "d/m/Y") . " " . substr($dato['con_horafin'], 0, 5) ?></td>
-                                    <td>
-                                        <ul class="list-group list-group-flush">
-                                            <?php
-                                            foreach ($datos as $dat) {
-                                                if ($dato['con_id'] == $dat['con_id']) {
-                                                    echo '<li>' . $dat['mod_abreviatura'] . " " . $dat['niv_descripcion'] . ($dat['esp_descripcion'] != "-" ? " " . $dat['esp_descripcion'] : "") . '</li>';
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead style="background-color:red" class="text-white">
+                            <tr>
+                                <!-- <th class="text-center">#</th>-->
+                                <th class="text-center">NÚMERO</th>
+                                <th class="text-center">TIPO</th>
+                                <th class="text-center">FECHA DE INICIO</th>
+                                <th class="text-center">FECHA DE FIN</th>
+                                <th class="text-center">GRUPOS DE INSCRIPCIÓN</th>
+                                <th class="text-center">ESTADO</th>
+                                <th class="text-center">ACCIONES</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $arrUnicos = [];
+                            $i = 0;
+                            foreach ($datos as $dato) {
+                                if (!in_array($dato['con_id'], $arrUnicos)) {
+                                    $arrUnicos[] = $dato['con_id'];
+                                    $cadena = $dato['con_id'] . "||0";
+                            ?>
+                                    <tr>
+                                        <!--<td class="text-center"><b><?= $i + 1; ?></b></td>-->
+                                        <td class="text-center"><b><?= "CONV-" . sprintf('%04d', $dato['con_numero']) . "-" . $dato['con_anio'] ?></b></td>
+                                        <td class="text-center"><?= $dato['descripcion'] ?></td>
+                                        <td class="text-center"><?= format_date($dato['con_fechainicio'], "d/m/Y") . " " . substr($dato['con_horainicio'], 0, 5) ?> </td>
+                                        <td class="text-center"><?= format_date($dato['con_fechafin'], "d/m/Y") . " " . substr($dato['con_horafin'], 0, 5) ?></td>
+                                        <td>
+                                            <ul class="list-group list-group-flush">
+                                                <?php
+                                                foreach ($datos as $dat) {
+                                                    if ($dato['con_id'] == $dat['con_id']) {
+                                                        echo '<li>' . $dat['mod_abreviatura'] . " " . $dat['niv_descripcion'] . ($dat['esp_descripcion'] != "-" ? " " . $dat['esp_descripcion'] : "") . '</li>';
+                                                    }
                                                 }
-                                            }
-                                            ?>
-                                        </ul>
-                                    </td>
-                                    <td class="text-center">
-                                        <span class=""><?php echo $dato['con_estado'] ? 'ABIERTO' : 'CERRADO' ?> </span>
-                                    </td>
-                                    <td class="text-center">
-                                        <div class="d-flex justify-content-center gap-2">
-                                            <?php if ($dato['con_tipo'] == 2  || $dato['con_tipo'] == 1 ) { ?>
-                                                <a type="button" class="btn btn-sm btn-danger <?= $dato['con_estado'] ? '' : 'disabled' ?>" title="Ingresar a postular" data-bs-toggle="modal" data-bs-target="#postularModal" data-conid="<?= $dato['con_id'] ?>" data-contitle="<?= "CONV-" . sprintf('%04d', $dato['con_numero']) . "-" . $dato['con_anio']  ?>" <b><i class="fa-solid fa-arrow-right-to-bracket fa-2xl"></i>POSTULAR</b>
-                                                </a>
-                                            <?php } ?>
-                                            
-                                        </div>
-                                    </td>
-                                </tr>
-                        <?php $i++;
-                            }
-                        } ?>
-                    </tbody>
-                </table>
-            </div>
+                                                ?>
+                                            </ul>
+                                        </td>
+                                        <td class="text-center">
+                                            <span class=""><?php echo $now_unix >=  $dato['con_fechainicio'] &&  $now_unix  <=  $dato['con_fechainicio']   ? 'ABIERTO' : 'CERRADO' ?> </span>
+                                        </td>
+                                        <td class="text-center">
+                                            <div class="d-flex justify-content-center gap-2">
+                                                <?php if ($dato['con_tipo'] == 2  || $dato['con_tipo'] == 1) { ?>
+                                                    <a type="button" class="btn btn-sm btn-danger <?= $now_unix >=  $dato['con_fechainicio'] &&  $now_unix  <=  $dato['con_fechainicio']  ? '' : 'disabled' ?>" title="Ingresar a postular" data-bs-toggle="modal" data-bs-target="#postularModal" data-conid="<?= $dato['con_id'] ?>" data-contitle="<?= "CONV-" . sprintf('%04d', $dato['con_numero']) . "-" . $dato['con_anio']  ?>" <b><i class="fa-solid fa-arrow-right-to-bracket fa-2xl"></i>POSTULAR</b>
+                                                    </a>
+                                                <?php } ?>
+
+                                            </div>
+                                        </td>
+                                    </tr>
+                            <?php $i++;
+                                }
+                            } ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
