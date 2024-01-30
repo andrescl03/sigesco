0fa7e98f878b82b0194e4094c912a378d80fec84
ext.dcarrion <andrescarrion199603@gmail.com>
2024-01-30 11:08:18 -0500

    log

---
+++ application/logs/log-2024-01-30.php
@@ -0,0 +1,53 @@
+ERROR - 2024-01-30 10:49:51 --> Severity: Notice --> Array to string conversion C:\xampp\htdocs\sigesco\application\controllers\PostulacionesWeb.php 27
+DEBUG - 2024-01-30 16:50:02 --> Accessing maintenance hook!
+DEBUG - 2024-01-30 16:50:02 --> UTF-8 Support Enabled
+DEBUG - 2024-01-30 16:50:02 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2024-01-30 16:50:02 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2024-01-30 16:50:02 --> Tools class already loaded. Second attempt ignored.
+DEBUG - 2024-01-30 16:50:02 --> Tools class already loaded. Second attempt ignored.
+ERROR - 2024-01-30 10:50:02 --> Severity: Notice --> Array to string conversion C:\xampp\htdocs\sigesco\application\controllers\PostulacionesWeb.php 27
+DEBUG - 2024-01-30 10:50:02 --> Log detalle => "Ingresa a busquedaArray"
+DEBUG - 2024-01-30 10:50:02 --> Total execution time: 0.0383
+DEBUG - 2024-01-30 16:50:47 --> Accessing maintenance hook!
+DEBUG - 2024-01-30 16:50:47 --> UTF-8 Support Enabled
+DEBUG - 2024-01-30 16:50:47 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2024-01-30 16:50:47 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2024-01-30 16:50:47 --> Tools class already loaded. Second attempt ignored.
+DEBUG - 2024-01-30 16:50:47 --> Tools class already loaded. Second attempt ignored.
+DEBUG - 2024-01-30 10:50:47 --> Log detalle => Ingresa a busqueda{"documento":"07054303","convocatoria_id":"10","inscripcion_id":"1"}
+DEBUG - 2024-01-30 10:50:47 --> Total execution time: 0.0313
+DEBUG - 2024-01-30 16:55:58 --> Accessing maintenance hook!
+DEBUG - 2024-01-30 16:55:58 --> UTF-8 Support Enabled
+DEBUG - 2024-01-30 16:55:58 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2024-01-30 16:55:58 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2024-01-30 10:55:58 --> Tools class already loaded. Second attempt ignored.
+DEBUG - 2024-01-30 10:55:58 --> Total execution time: 0.0177
+DEBUG - 2024-01-30 16:55:58 --> Accessing maintenance hook!
+DEBUG - 2024-01-30 16:55:58 --> UTF-8 Support Enabled
+DEBUG - 2024-01-30 16:55:58 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2024-01-30 16:55:58 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2024-01-30 10:55:58 --> Tools class already loaded. Second attempt ignored.
+DEBUG - 2024-01-30 10:55:58 --> Total execution time: 0.0341
+DEBUG - 2024-01-30 16:55:58 --> Accessing maintenance hook!
+DEBUG - 2024-01-30 16:55:58 --> Accessing maintenance hook!
+DEBUG - 2024-01-30 16:55:58 --> Accessing maintenance hook!
+DEBUG - 2024-01-30 16:55:58 --> UTF-8 Support Enabled
+DEBUG - 2024-01-30 16:55:58 --> UTF-8 Support Enabled
+DEBUG - 2024-01-30 16:55:58 --> UTF-8 Support Enabled
+DEBUG - 2024-01-30 16:55:58 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2024-01-30 16:55:58 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2024-01-30 16:55:58 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2024-01-30 16:55:58 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2024-01-30 16:55:58 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2024-01-30 16:55:58 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2024-01-30 10:55:58 --> Total execution time: 0.0156
+DEBUG - 2024-01-30 10:55:58 --> Total execution time: 0.0161
+DEBUG - 2024-01-30 10:55:58 --> Total execution time: 0.0182
+DEBUG - 2024-01-30 16:56:00 --> Accessing maintenance hook!
+DEBUG - 2024-01-30 16:56:00 --> UTF-8 Support Enabled
+DEBUG - 2024-01-30 16:56:00 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2024-01-30 16:56:00 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2024-01-30 16:56:00 --> Tools class already loaded. Second attempt ignored.
+DEBUG - 2024-01-30 16:56:00 --> Tools class already loaded. Second attempt ignored.
+DEBUG - 2024-01-30 10:56:00 --> Log detalle => Ingresa a busqueda{"documento":"07054303","convocatoria_id":"10","inscripcion_id":"1"}
+DEBUG - 2024-01-30 10:56:00 --> Total execution time: 0.0291
