4ae2d1930461ef81148313f22f44bd959fcb3bed
ext.dcarrion <andrescarrion199603@gmail.com>
2024-02-16 00:33:47 -0500

    update

--- application/views/adjudicacion/index.php
+++ application/views/adjudicacion/index.php
@@ -10,11 +10,10 @@
         <div class="col-md-12">
             <div class="tile">
                 <div class="tile-body">
-                    <div class="card border-secondary" >
+                    <div class="card border-secondary">
                         <div class="card-body text-dark">
                             <div class="text-right mb-2">
                                 <div class="row">
-
                                     <div class="col-sm-12">
                                         <div class="card border bg-light">
                                             <div class="card-body" style="padding: 0.8rem 1rem;">
@@ -22,41 +21,53 @@
                                                     <div class="col-sm-12 ">
                                                         <div class="d-flex align-content-start flex-wrap gap-3">
                                                             <div class="d-grid gap-2 col-sm-2">
-                                                                <a type="button" href="<?php echo base_url(); ?>adjudicaciones/create" class="btn btn-outline-primary btn-sm" >
+                                                                <a type="button" href="<?php echo base_url(); ?>adjudicaciones/create" class="btn btn-outline-primary btn-sm">
                                                                     <b><i class="fa-solid fa-file-signature fa-lg"></i> Agregar</b>
                                                                 </a>
                                                             </div>
                                                             <div class="vr"></div>
-                                                            <div class="col-sm-6"><input type="text" class="form-control form-control-sm" id="txt_buscador" placeholder="Buscar..." ></div>
+                                                            <div class="col-sm-6">
+                                                                <div class="input-group">
+
+                                                                    <input type="text" class="form-control form-control-sm" id="txt_buscador" placeholder="Buscar...">
+                                                                    <button type="button" class="input-group-text btn btn-sm btn-primary shadow-none btn-search">Buscar</button>
+                                                                </div>  
+
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>  
-                                        </div> 
+                                            </div>
+                                        </div>
                                     </div>
-                                                                            
+
                                     <div class="col-sm-12">
                                         <div class="table-responsive">
                                             <table class="table table-hover" id="tableIndexAdjudicacion" style="width:100%; margin:0px;">
                                                 <thead>
                                                     <tr class="cabecera_tabla_2">
                                                         <th class="text-center">ID</th>
-                                                        <th class="text-center">PLAZA</th>
+                                                        <th class="text-center">CÓDIGO DE PLAZA</th>
+                                                        <th class="text-center">DOCUMENTO</th>
                                                         <th class="text-center">DOCENTE</th>
                                                         <th class="text-center">FECHA DE INICIO</th>
                                                         <th class="text-center">FECHA DE FIN</th>
                                                         <th class="text-center">FECHA DE REGISTRO</th>
+                                                        <th class="text-center">IE</th>
+                                                        <th class="text-center">MODALIDAD</th>
+                                                        <th class="text-center">NIVEL</th>
+                                                        <th class="text-center">ESPECIALIDAD</th>
                                                         <th class="text-center">DOCUMENTOS</th>
-                                                        <th class="text-center">ACCIONES</th>
+                                                        <th class="text-center">ACCIONES</th> 
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                
+
                                                 </tbody>
                                             </table>
                                         </div>
                                     </div>
-                                    
+
                                 </div>
                             </div>
                         </div>
