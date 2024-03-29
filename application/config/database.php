923c7549d9311973b93de9d918a5ab4cb4bd0408
andrescl03 <andrescarrion199603@gmail.com>
2024-03-29 17:55:06 -0500

    test2

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost', //bd
+	'hostname' => 'localhost', //bd2
 	'username' => 'root',
 	'password' => '',
 	'database' => 'sigesco',
