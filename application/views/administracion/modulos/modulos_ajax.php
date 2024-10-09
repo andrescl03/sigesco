a412ed6287e1d6aa1889b31beebbcfb6d1d10a85
joseant1215 <80193930+joseant1215@users.noreply.github.com>
2024-10-08 23:41:50 -0500

    docentes y el menu dinamico

--- application/views/administracion/modulos/modulos_ajax.php
+++ application/views/administracion/modulos/modulos_ajax.php
@@ -1,7 +1,41 @@
 <?php if(isset($mensaje["error"])) { ?> 
 	<div class="alert alert-danger" role="alert"><?php echo $mensaje["error"]; ?></div>		
 <?php }else{ ?>	
-	<?php //writer($datos); ?>
+	<?php
+		function tieneHijos($menu, $parentId) {
+			foreach ($menu as $item) {
+				if ($item['mdl_hijode'] == $parentId) {
+					return true;
+				}
+			}
+			return false;
+		}			
+		function imprimirMenu($menu, $parentId = 0, $nivel = 1) {
+			foreach ($menu as $item) {
+				if ($item['mdl_hijode'] == $parentId) {
+					// Agregar margen basado en el nivel para diferenciar hijos de padres
+					$marginLeft = $nivel * 15; // 20px de margen por nivel
+					$esPadre = tieneHijos($menu, $item['mdl_id']);
+					$nombreEstilado = $esPadre ? "<strong>{$item['mdl_nombre']}</strong>" : $item['mdl_nombre'];
+					echo "<tr>";
+					echo "<td>{$item['mdl_orden']}</td>";
+					// Primera columna: nombre con margen y negrita si es un padre
+					echo "<td style='padding-left: {$marginLeft}px;'>{$nombreEstilado}</td>";
+					// Segunda columna: otra información
+					echo "<td>{$item['mdl_ruta']}</td>";
+					echo '<td class="text-center">'.$item['mdl_icono'].'</td>';
+					echo '<td class="text-center">'.$item['hijo'].'</td>';
+					echo '<td class="text-center">'. ($item['mdl_estado']==1?'<span class="badge bg-success">Activo</span>':'<span class="badge bg-danger">Inactivo</span>') .'</td>';
+					echo '<td class="text-center"><a type="button" class="badge bg-info text-white btn_editarModulos" id="'.$item['mdl_id'].'"><i class="far fa-edit fa-lg"></i></a>
+							<a type="button" class="badge bg-danger text-white btn_eliminarModulos" id="'.$item['mdl_id'].'"><i class="far fa-trash-alt fa-lg"></i></a>	
+						 </td>';
+					echo "</tr>";
+					// Llamada recursiva para los hijos de este elemento
+					imprimirMenu($menu, $item['mdl_id'], $nivel + 1);
+				}
+			}
+		}
+	?>
 	<div class="tresponsive">
 	<table id="tb_listadoModulos" class="table-condensed table-bordered table-hover table-sm" cellspacing="0" width="100%" style="font-size:13px;">
 		<thead>
@@ -16,8 +50,9 @@
 			</tr>
 		</thead>
 		<tbody>
+			<?php imprimirMenu(json_decode(json_encode($datos), true)); ?>
 			<?php $i=0; foreach ($datos as $dato) { ?>
-			<tr>										
+			<!-- <tr>										
 				<td class="text-center"><b><?php echo $i+1;?></b></td>
 				<td>
 					<?php 
@@ -30,19 +65,14 @@
 				</td>
 				<td><?php echo $dato->mdl_ruta;?></td>
 				<td><?php echo $dato->mdl_icono;?></td>
-				<td class="text-center"><?php  echo $dato->hijo;
-						/*if($dato->mdl_hijode!=0){					
-							echo $datos[$dato->mdl_hijode-6]->mdl_nombre; // se hizo porque los 5 registros de administrador y de ids continuos no debe aparecer (tambien esta restringido en la bd)
-						}*/
-					?>
+				<td class="text-center"><?php  echo $dato->hijo; ?>
 				</td>				
 				<td class="text-center"><?php echo ($dato->mdl_estado==1?'<span class="badge bg-success">Activo</span>':'<span class="badge bg-danger">Inactivo</span>')?></td>				
 				<td class="text-center">
 					<a type="button" class="badge bg-info text-white btn_editarModulos" id="<?php echo $dato->mdl_id;?>"><i class="far fa-edit fa-lg"></i></a>
 					<a type="button" class="badge bg-danger text-white btn_eliminarModulos" id="<?php echo $dato->mdl_id;?>"><i class="far fa-trash-alt fa-lg"></i></a>	
 				</td>
-				
-			</tr>
+			</tr> -->
 			<?php $i++; } ?>  								
 		</tbody>							
 	</table>
